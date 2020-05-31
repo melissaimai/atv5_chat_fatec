99f386f2a7467857f2aa2539214677c6ec4753db
marcos <marcos.teixeira@cittamobi.com.br>
2020-05-31 12:27:58 -0300

    MainActivity

--- app/src/main/java/br/gov/sp/fatec/fatec_ipi_noite_chat_com_firebase/MainActivity.java
+++ app/src/main/java/br/gov/sp/fatec/fatec_ipi_noite_chat_com_firebase/MainActivity.java
@@ -3,12 +3,15 @@ package br.gov.sp.fatec.fatec_ipi_noite_chat_com_firebase;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import br.gov.sp.fatec.fatec_ipi_noite_chat_com_firebase.util.ElementEnabler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         loginEditText = findViewById(R.id.loginEditText);
         senhaEditText = findViewById(R.id.senhaEditText);
+        Button loginButton = findViewById(R.id.loginButton);
+        ElementEnabler.textViews(loginButton, loginEditText, senhaEditText);
 
         firebaseAuth = FirebaseAuth.getInstance();
     }
@@ -34,14 +39,11 @@ public class MainActivity extends AppCompatActivity {
     public void fazerLogin (View view){
         String login = loginEditText.getText().toString();
         String senha = senhaEditText.getText().toString();
-        firebaseAuth.signInWithEmailAndPassword(login, senha).addOnSuccessListener((result) -> {
+        firebaseAuth.signInWithEmailAndPassword(login, senha).addOnSuccessListener(result -> {
             Toast.makeText(this, getString(R.string.login_sucesso), Toast.LENGTH_LONG).show();
             startActivity(new Intent(this, MenuActivity.class));
             finish();
-        }).addOnFailureListener((error) -> {
-            Toast.makeText(this, getString(R.string.login_falhou), Toast.LENGTH_LONG).show();
-            finish();
-        });
+        }).addOnFailureListener(error -> Toast.makeText(this, getString(R.string.login_falhou), Toast.LENGTH_LONG).show());
     }
 }
 
