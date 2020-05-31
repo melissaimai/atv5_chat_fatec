2c2a07e32e37638da584a927bfe6aa83136140dd
marcos <marcos.teixeira@cittamobi.com.br>
2020-05-31 12:34:48 -0300

    NovoUsuarioActivity

--- app/src/main/java/br/gov/sp/fatec/fatec_ipi_noite_chat_com_firebase/NovoUsuarioActivity.java
+++ app/src/main/java/br/gov/sp/fatec/fatec_ipi_noite_chat_com_firebase/NovoUsuarioActivity.java
@@ -2,12 +2,15 @@ package br.gov.sp.fatec.fatec_ipi_noite_chat_com_firebase;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import br.gov.sp.fatec.fatec_ipi_noite_chat_com_firebase.util.ElementEnabler;
 
 public class NovoUsuarioActivity extends AppCompatActivity {
 
@@ -23,21 +26,18 @@ public class NovoUsuarioActivity extends AppCompatActivity {
 
         loginNovoUsuarioEditText = findViewById(R.id.loginNovoUsuarioEditText);
         senhaNovoUsuarioEditText = findViewById(R.id.senhaNovoUsuarioEditText);
+        Button signupButton = findViewById(R.id.signupButton);
+        ElementEnabler.textViews(signupButton, loginNovoUsuarioEditText, senhaNovoUsuarioEditText);
 
         firebaseAuth = FirebaseAuth.getInstance();
     }
 
-    public void criarNovoUsuario (View view){
+    public void criarNovoUsuario (View view) {
         String login = loginNovoUsuarioEditText.getText().toString();
         String senha = senhaNovoUsuarioEditText.getText().toString();
-        firebaseAuth.createUserWithEmailAndPassword(login, senha)
-                .addOnSuccessListener((result) -> {
-                    Toast.makeText(this, getString(R.string.cadastro_sucesso), Toast.LENGTH_SHORT).show();
-                    finish();
-                }).addOnFailureListener((erro) -> {
-                    Toast.makeText(this, getString(R.string.cadastro_falha), Toast.LENGTH_SHORT).show();
-                    finish();
-                });
-
+        firebaseAuth.createUserWithEmailAndPassword(login, senha).addOnSuccessListener(result -> {
+            Toast.makeText(this, getString(R.string.cadastro_sucesso), Toast.LENGTH_SHORT).show();
+            finish();
+        }).addOnFailureListener(erro -> Toast.makeText(this, getString(R.string.cadastro_falha), Toast.LENGTH_SHORT).show());
     }
 }
